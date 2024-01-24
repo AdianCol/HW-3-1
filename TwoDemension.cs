7ed94a003beb7a4cf5c96b57f8e9c5a1202d1269
AdianCol <156536098+AdianCol@users.noreply.github.com>
2024-01-24 19:25:02 +0300

    Update TwoDemension.cs

--- TwoDemension.cs
+++ TwoDemension.cs
@@ -30,7 +30,7 @@ public class MultiDimensionalArray
 
     public void FillWithUserInput()
     {
-        Console.WriteLine("Enter the elements of the array (row by row, separated by space):");
+        Console.WriteLine("Enter the elements of the array:");
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
